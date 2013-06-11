08c9f6d9ead7ed4b35109743ebca823b07c8150c
Thanassis Tsiodras <ttsiodras@gmail.com>
2013-06-11 16:21:05 +0300

    Refactored, so it can be used in standalone form, too. Bonus point: fed by default via yielded stdin :-)

--- stats.py
+++ stats.py
@@ -1,37 +1,69 @@
 #!/usr/bin/env python
-import math
-import sys
-green = chr(27)+"[32m" if sys.stdout.isatty() else ""
-normal = chr(27)+"[0m" if sys.stdout.isatty() else ""
-total = totalSq = n = 0
-allOfThem = []
-while True:
-    try:
-        a = float(raw_input())
-    except:
-        break
-    total += a
-    totalSq += a*a
-    n += 1
-    allOfThem.append(a)
-if n in [0, 1]:
-    print "%s data point read... aborting." % (
-        "only one" if n == 1 else "no")
-    sys.exit(1)
 
-varianceFull = (totalSq - total*total/n)/n
-variance = (totalSq - total*total/n)/(n-1)
-srted = sorted(allOfThem)
-measurements = [
-    ("Elements", n),
-    ("Average value", total/n),
-    ("Std deviation", math.sqrt(varianceFull)),
-    ("Sample stddev", math.sqrt(variance)),
-    ("Median", srted[len(allOfThem)/2]),
-    ("Min", srted[0]),
-    ("Max", srted[-1]),
-    (green+"Overall", (str(total/n)+" +/- "+"%2.1f%%"+normal) %
-        (100*math.sqrt(variance)*n/total))
-]
-for label, value in measurements:
-    print "%*s:" % (15, label), value
+import math
+
+from sys import stdout
+
+# Colored message ANSI constants
+g_green = chr(27) + "[32m" if stdout.isatty() else ""
+g_yellow = chr(27) + "[33m" if stdout.isatty() else ""
+g_normal = chr(27) + "[0m" if stdout.isatty() else ""
+
+
+def printStatsOfList(results, label='Statistics', summaryOnly=False):
+    total = totalSq = n = 0
+    allOfThem = []
+    for a in results:
+        total += a
+        totalSq += a*a
+        n += 1
+        allOfThem.append(a)
+    if n == 0:
+        return
+    varianceFull = (totalSq - total*total/n)/n
+    if varianceFull < 0.:
+        varianceFull = 0.
+    if n > 1:
+        variance = (totalSq - total*total/n)/(n-1)
+        if variance < 0.:
+            variance = 0.
+    else:
+        variance = 0.
+    srted = sorted(allOfThem)
+    if summaryOnly:
+        s = g_green + ("%6.2f" % (total/n)) + " +/- " + "%6.2f%%" + g_normal
+        print s % ((100*math.sqrt(variance)*n/total) if total > 0 else 0.),
+    else:
+        print "\n", g_yellow+label+g_normal, ":"
+        samplesNo = len(allOfThem)
+        measurements = [
+            ("Average value", total/n),
+            ("Std deviation", math.sqrt(varianceFull)),
+            ("Sample stddev", math.sqrt(variance)),
+            ("Median",
+                srted[samplesNo/2]
+                if samplesNo % 2
+                else 0.5*(srted[samplesNo/2 - 1] + srted[samplesNo/2])),
+            ("Min", srted[0]),
+            ("Max", srted[-1]),
+            (g_green+"Overall", (str(total/n)+" +/- "+"%2.1f%%"+g_normal) %
+                ((100*math.sqrt(variance)*n/total) if total > 0 else 0.))
+        ]
+        for label, value in measurements:
+            print "%*s:" % (15, label),
+            if isinstance(value, str):
+                print value
+            else:
+                print "%.5f" % value
+
+
+def readListOfIntegersOrFloatsFromStdin():
+    while True:
+        try:
+            a = float(raw_input())
+            yield a
+        except:
+            break
+
+if __name__ == "__main__":
+    printStatsOfList(readListOfIntegersOrFloatsFromStdin())
