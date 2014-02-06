ccc59b3fefd30d14af0aefb5fdce60241c3b65be
Thanassis Tsiodras <ttsiodras@gmail.com>
2014-02-06 06:35:32 +0200

    Copy-paste typos

--- archLinux.orphans.ordered.by.size.sh
+++ archLinux.orphans.ordered.by.size.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pacman -Qdt | while read NAME FOO ; do echo -n $NAME ; grep -A1 SIZE /var/lib/pacman/local/${NAME}*/desc | tr -d 012 ; echo ; done | sed s,.SIZE., , | sort -n -k 2
+pacman -Qdt | while read NAME FOO ; do echo -n $NAME ; grep -A1 SIZE /var/lib/pacman/local/${NAME}*/desc | tr -d '\012' ; echo ; done | sed 's,.SIZE., ,' | sort -n -k 2
