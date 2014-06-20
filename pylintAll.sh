5d8d8f6e5a7c0b794e45bca569c86e89c92ce846
Thanassis Tsiodras <ttsiodras@gmail.com>
2014-06-20 11:16:04 +0300

    Might as well do it properly

--- pylintAll.sh
+++ pylintAll.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-for i in "$@" ; do
+#
+# I run pylint a lot - this eases the pain
+#
+FILES=${@:-$(echo *.py)}
+for i in $FILES ; do
     echo -n "$(printf "%30s :  " "$i")" 
     pylint "$i" | grep rated 
 done
