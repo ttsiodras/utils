5bc69055918197266038e27f81c19d436d25686e
Thanassis Tsiodras <ttsiodras@gmail.com>
2014-06-23 10:21:29 +0300

    Sum the pdfs' page counts

--- pdfCountPages.sh
+++ pdfCountPages.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-#pdftotext "$1" /dev/stdout | grep '' | wc -l
-pdfinfo "$1" | grep ^Pages
+#pdftotext "$1" /dev/stdout | grep '
+for i in "$@" ; do
+    pdfinfo "$i" | grep ^Pages
+done | awk '{a+=$2; print a}'
