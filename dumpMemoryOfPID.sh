8ceadcc4254c296ad2db26cd251b3730adbd55fa
Thanassis Tsiodras <ttsiodras@gmail.com>
2014-06-24 14:11:27 +0300

    dump the memory spaces of any app

---
+++ dumpMemoryOfPID.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# 
+# As root, dump the memory spaces of any app
+# (thanks to SO)
+#
+grep rw-p /proc/$1/maps | \
+    sed -n 's/^\([0-9a-f]*\)-\([0-9a-f]*\) .*$/\1 \2/p' | \
+    while read start stop
+    do
+        gdb --batch --pid $1 -ex "dump memory $1-$start-$stop.dump 0x$start 0x$stop"
+    done
