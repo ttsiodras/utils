c0ceecd24ebb86440a8f52a5d93a20d532887f15
Thanassis Tsiodras <ttsiodras@gmail.com>
2014-08-22 13:20:06 +0300

    Moved to fd 16

--- flashVideo.sh
+++ flashVideo.sh
@@ -18,10 +18,10 @@ else
 fi
 BUFFER1="/dev/shm/$FILENAME"
 BUFFER2="${BUFFER1}.new"
-cat /proc/$PID/fd/15 > "$BUFFER1"
+cat /proc/$PID/fd/16 > "$BUFFER1"
 while true ; do
     sleep 5
-    cat /proc/$PID/fd/15 > "$BUFFER2"
+    cat /proc/$PID/fd/16 > "$BUFFER2"
     if [ $(stat -c '%s' "$BUFFER1") -lt $(stat -c '%s' "$BUFFER2") ] ; then
         mv "$BUFFER2" "$BUFFER1"
         ls -l "$BUFFER1"
