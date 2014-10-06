2d24eac4e2988a9558ad91c56ad2c6e33e86476b
Tim Stackhouse <tstackhouse@ahint.com>
2014-10-06 16:12:53 -0400

    Added -empty option to find to supress 'non-empty directory' warnings from rm

--- rmdirRecursive.sh
+++ rmdirRecursive.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # After invoking cmpDir1andDir2andRemoveIdenticalFromDir1, 
 # I usually need to clean up empty folders - this is how I do it.
-find . -depth -type d -exec rmdir '{}' ';'
+find . -depth -type d -empty -exec rmdir '{}' ';'
