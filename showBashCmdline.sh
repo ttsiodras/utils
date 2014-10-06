3ed6972419570cbe5212ca6afb2eabeb5e300398
Thanassis Tsiodras <ttsiodras@gmail.com>
2014-10-06 12:25:01 +0300

    Properly handle the case of delete /bin/bash (due to updates - see my blog post)

--- showBashCmdline.sh
+++ showBashCmdline.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 #
-# I asked a question on stackoverflow about getting the currently
-# executing (long!) cmdline inside a long-running screen-ed bash.
+# Show the currently executing command line inside a bash shell.
 #
-#   http://unix.stackexchange.com/questions/159010/how-can-i-see-the-exact-command-line-being-executed-inside-some-bash-instance#159010
-#
-# ...and got nowhere - the guys there gave me suggestions that simply don't work.
-#
-# So after a bit of hacking, I completed this script - I use GDB
-# to save the bash history, and tail -1 on it :-)
+# Relevant blog post:
+#    http://users.softlab.ece.ntua.gr/~ttsiod/bashheimer.html
 #
 if [ $# -ne 1 ] ; then
     echo Usage: $0 BASH_PID
@@ -18,16 +13,15 @@ if [ ! -d /proc/$1 ] ; then
     echo There is no such PID
     exit 1
 fi
-if [ $(realpath /proc/$1/exe) != "/bin/bash" -a \
-     $(realpath /proc/$1/exe) != "/usr/bin/bash" -a \
-     $(realpath /proc/$1/exe) != "/usr/local/bin/bash" ] ; then
+STATNAME=$(stat --format='%N' /proc/$1/exe)
+if [[ $STATNAME != */bin/bash* ]] ; then
     echo This is not a bash PID
     exit 1
 fi
-gdb --pid $1 >/dev/null 2>&1 <<OEF
+gdb --pid $1 /proc/$1/exe >/dev/null 2>&1 <<OEF
 call write_history("/tmp/bash_history_recover")
 detach
 q
 OEF
-tail -1 /tmp/bash_history_recover
+tail -1 /tmp/bash_history_recover 2>/dev/null
 rm -f /tmp/bash_history_recover
