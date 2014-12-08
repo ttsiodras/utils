69177146eaa2e8497f8fe688400c3fed015a592f
Thanassis Tsiodras <ttsiodras@gmail.com>
2014-12-08 20:52:42 +0200

    On occasion, xclip blocks for ever - race condition in X Windows

--- clipboardDaemon.py
+++ clipboardDaemon.py
@@ -2,32 +2,43 @@
 '''
 I've had it with syncing X and the clipboard.
 I was using autocutsel, but sometimes it doesn't work -
-replaced it with this quick hack. 
-
-Seems to work perfectly so far... Needed 'awk 1' to add
-a newline when it was missing.
+replaced it with this quick hack.
 '''
 import os
 import time
 from collections import namedtuple
 
+
 make_state = namedtuple("State", ["xwin", "clipboard"])
-getX = "xclip -selection primary -o 2>/dev/null"
-getClip = "xclip -selection clipboard -o 2>/dev/null"
-setX = getX.replace("-o", "-i")
-setClip = getClip.replace("-o", "-i")
-getState = lambda: make_state(os.popen(getX).read(), os.popen(getClip).read())
+# On occasion, xclip blocks for ever - apparently some race
+# condition in XWindows. GNU timeout to the rescue...
+xclipCmd = "timeout 2 xclip -selection %s -o 2>/dev/null"
+getX = xclipCmd % "primary"
+getClip = xclipCmd % "clipboard"
+# xclip does not (always) close STDOUT...
+setX = getX.replace("-o", "-i >/dev/null")
+setClip = getClip.replace("-o", "-i >/dev/null")
+getState = lambda: make_state(
+    os.popen(getX).read(),
+    os.popen(getClip).read())
 state = getState()
 while True:
+    debug = os.path.exists("/tmp/wtf")
     newState = getState()
+    if debug:
+        print "newState:", newState
     if newState.xwin != state.xwin:
         print newState
         print "Syncing X to clipboard"
-        os.system(getX + "|perl -i -p0777we's/\n\z//'|" + setClip)
+        # The perl below strips the last newline
+        os.system(
+            getX + r"|perl -i -p0777we's/\n\z//'|" + setClip)
         state = getState()
     elif newState.clipboard != state.clipboard:
         print newState
         print "Syncing clipboard to X"
-        os.system(getClip + "|perl -i -p0777we's/\n\z//'|" + setX)
+        # The perl below strips the last newline
+        os.system(
+            getClip + r"|perl -i -p0777we's/\n\z//'|" + setX)
         state = getState()
     time.sleep(0.25)
