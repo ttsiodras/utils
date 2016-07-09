9b44362d8fee53b818ad879e2423ad5e80eb8883
Thanassis Tsiodras <ttsiodras@gmail.com>
2016-07-09 10:00:27 +0200

    Extract the audio contents of any media file to a LAME-encoded .mp3

---
+++ any2mp3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Extract the audio contents of any media file to a LAME-encoded .mp3
+#
+if [ $# -ne 1 ] ; then
+	echo Usage: "$0" file output.mp3
+	exit 1
+fi
+mplayer -ao pcm:file=/dev/shm/test.$$.wav -vo null -vc dummy -benchmark "$1"
+cd /dev/shm || exit 1
+lame test.$$.wav
+rm test.$$.wav
+cd - || exit 1
+mv /dev/shm/test.$$.mp3 "$2"
