e1502d62b42aef911dd6adfd6463287b833611fa
Thanassis Tsiodras <ttsiodras@gmail.com>
2016-07-14 22:39:55 +0200

    Don't depend on my 'nth' script - just use awk

--- jpg2lep.sh
+++ jpg2lep.sh
@@ -4,8 +4,8 @@
 # to save space in stored JPEG images without any loss.
 for i in "$@" ; do
     lepton "$i" /dev/shm/lepton.$$ || { echo "$i" Failed... ; continue ; }
-    DST=$(lepton /dev/shm/lepton.$$ /dev/stdout | md5sum - | nth 0)
-    SRC=$(cat "$i" | md5sum - | nth 0)
+    DST=$(lepton /dev/shm/lepton.$$ /dev/stdout | md5sum - | awk '{print $1}')
+    SRC=$(md5sum "$i" | awk '{print $1}')
     if [ "$SRC" == "$DST" ] ; then
         mv /dev/shm/lepton.$$ "$i".lep
     fi
