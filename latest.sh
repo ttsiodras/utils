dc62be10af72f8aee06a83c009c0e90e74d5d054
Thanassis Tsiodras <ttsiodras@gmail.com>
2017-05-07 17:20:30 +0200

    Don't cross filesystems

--- latest.sh
+++ latest.sh
@@ -3,4 +3,4 @@
 # UNIX power - makes latest.py obsolete.
 #
 DIR=${1:-.}
-find "${DIR}" ! -type d -printf "%T+ %11s %p\\n" | sort -n
+find "${DIR}" -xdev ! -type d -printf "%T+ %11s %p\\n" | sort -n
