9c438614699b9f95949d046a2a1b78d2896ff89a
Thanassis Tsiodras <ttsiodras@gmail.com>
2020-02-01 14:20:42 +0100

    Pipe and interactively exclude content.

---
+++ filterExclude.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Pipe any kind of output to this, and you will be able
+# to exclude outputs interactively, filtering until you
+# are left with nothing.
+#
+# e.g. 
+#
+#   latest.py | filterExclude.sh
+#
+# ...will allow you to see the files you've created in 
+# the last few days/weeks, and filter out until you find
+# that big one you somehow inflicted on your filesystem
+# and forgot about.
+
+DATA=/dev/shm/data.$$
+function finish {
+    rm -f $DATA
+}
+trap finish EXIT
+# Read all stdin into a temp file that will be cleaned on exit
+cat > $DATA
+
+# And now keep reading from ACTUAL stdin (not the piped input)
+# and remove lines if they match the input.
+(
+GR=""
+tail -40 "$DATA"
+echo -n Exclude this or Ctrl-D ... 
+while read -r ANS ; do
+    [ -z "$GR" ] && GR="$ANS" || GR="$GR|$ANS"
+    grep -E -v "$GR" "$DATA" | tail -40
+done ) <&1
