d89e0813f62309538e12b02a8b32fcaaafb76519
Thanassis Tsiodras <ttsiodras@gmail.com>
2021-03-20 22:27:33 +0100

    Safely dispatch URLs sent from zathura to the firejail-ed Firefox.

--- youtubeFromClipboard.sh
+++ youtubeFromClipboard.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 LAST_URL=""
+SITES='youtube.com|framatube|vimeo.com|youtu.be|192.168.1.22|atomicpi'
+
 while true
 do
-    URL="$(timeout 2 xclip -o 2>/dev/null | grep ^htt | egrep 'youtube.com|framatube|vimeo.com|youtu.be|192.168.1.22|atomicpi' )" 
-    [ ! -z "$URL" ] && [ "$LAST_URL" != "$URL" ] && {
+    URL="$(timeout 2 xclip -o 2>/dev/null)" 
+    if [ "$LAST_URL" == "$URL" ] ; then
+        sleep 1 
+        if [ -f /tmp/stopyou ] ; then
+            break
+        else
+            continue
+        fi
+    fi
+    echo "$URL" | grep -E "$SITES" >/dev/null && {
         LAST_URL="$URL"
         echo '[-] Got URL: '"$URL"
         echo '[-] Cleaning clipboard...'
@@ -16,8 +26,28 @@ do
         done
         echo '[-] Clipboard cleaned, playing video...'
         echo "$URL"  > /dev/shm/last_video
-        mpv -fs "$URL" 
-    } 
+        WORKSPACE=$(i3-msg -t  get_workspaces |  jq 'map([.num, .visible])' | grep -B1 true | head -1 | sed 's/[, ]*//g')
+        i3-msg 'workspace 9; exec xterm -e mpv -fs "'"$URL"'"'
+        sleep 1
+        i3-msg "workspace ${WORKSPACE}"
+        continue
+    }
+    echo "$URL" | grep "^zathura: http" >/dev/null && {
+        LAST_URL="$URL"
+        URL="$(echo "$URL" | sed 's,^zathura: ,,')"
+        if pgrep firefox > /dev/null ; then
+            notify-send "Placed URL in clipboard, go to firefox!"
+            echo "$URL" | xclip
+        else
+            sudo su - browser -c "/home/browser/bin.local/firefox.sh \"$URL\"" &
+        fi
+        sleep 1 
+        if [ -f /tmp/stopyou ] ; then
+            break
+        else
+            continue
+        fi
+    }
     sleep 1 
     [ -f /tmp/stopyou ] && break
 done
