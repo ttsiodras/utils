dec110ad02cecf1f8b46e5facb7e6aa53751b575
Thanassis Tsiodras <ttsiodras@gmail.com>
2022-09-25 15:19:44 +0200

    No more egrep.

--- exclude.sh
+++ exclude.sh
@@ -14,6 +14,6 @@ tail -40 "$1"
 echo -n Exclude this or Ctrl-D ... 
 while read ANS ; do
     [ -z "$GR" ] && GR="$ANS" || GR="$GR|$ANS"
-    egrep -v "$GR" "$1" | tail -40
+    grep -E -v "$GR" "$1" | tail -40
     echo "-v $GR $1" > /dev/shm/filter
 done
