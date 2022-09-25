b072d47f153a02c5844970c97454d63a31e5e8a3
Thanassis Tsiodras <ttsiodras@gmail.com>
2022-09-25 15:19:14 +0200

    Python3

--- countMoviesLength.py
+++ countMoviesLength.py
@@ -1,4 +1,4 @@
-#!/home/ttsiod/.virtualenvs/flask/bin/python2
+#!/usr/bin/env python3
 """
 A custom script (tailor-made to my machines) that uses
 all the CPU cores of my machine to add up the lengths
@@ -13,7 +13,7 @@ from concurrent import futures
 def getMovieLength(filename):
     if not os.path.isfile(os.path.realpath(filename)):
         return filename, 0.
-    cmd = "mplayer -quiet -identify -frames 0 -vo null "
+    cmd = "mplayer -nosound -quiet -identify -frames 0 -vo null "
     cmd += "\"%s\" 2>/dev/null | grep LENGTH" % filename
     try:
         return filename, float(os.popen(cmd).readlines()[0].split('=')[1])
@@ -24,7 +24,7 @@ def getMovieLength(filename):
 def main():
     cmd = "cat /proc/cpuinfo  |grep ^processor | wc -l"
     totalCPUs = int(os.popen(cmd).readlines()[0])
-    print "   Movie (sec)     Running Total"
+    print("   Movie (sec)     Running Total")
     with futures.ProcessPoolExecutor(max_workers=totalCPUs) as executor:
         total = 0.
         for filename, movieLength in executor.map(
@@ -33,12 +33,12 @@ def main():
             totalHours = int(total / 3600)
             totalMinutes = int(total / 60) - totalHours*60
             totalSeconds = int(total) - totalHours*3600 - totalMinutes*60
-            print "%13.2f    %4d:%02d:%02d %s" % (
+            print("%13.2f    %4d:%02d:%02d %s" % (
                 movieLength,
                 totalHours,
                 totalMinutes,
                 totalSeconds,
-                filename)
+                filename))
     os.system("stty icanon echo")
 
 if __name__ == "__main__":
