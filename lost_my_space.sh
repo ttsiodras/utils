398bf802d01e9059ad4e098c77e15c94b4019711
Thanassis Tsiodras <ttsiodras@gmail.com>
2023-03-17 16:20:55 +0100

    Just show the rate of new lines updated, no need to spam

--- lost_my_space.sh
+++ lost_my_space.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /root/bin.local/ || exit 1
-python3 ../bin/lost_my_space.py "$@" | tee /dev/shm/lost
+python3 ../bin/lost_my_space.py "$@" | tee /dev/shm/lost | pv -l > /dev/null
 sort -n /dev/shm/lost > /dev/shm/a
 mv /dev/shm/a /dev/shm/lost 
 exclude.sh /dev/shm/lost
