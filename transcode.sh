e5b117e44ca8dff638ae3a38b6f061a3f862cbf8
Thanassis Tsiodras <ttsiodras@gmail.com>
2024-04-21 19:14:08 +0200

    CRF is much nicer. Keep info in comments about other functionality.

--- transcode.sh
+++ transcode.sh
@@ -5,12 +5,17 @@ if [ $# -ne 2 ] ; then
     echo Usage: $0 input_file output.mp4
     exit 1
 fi
+
+# If you need HW rescaling add this:
+# -vf scale_vaapi=w=3840:h=1920 \
+
+# If you need to control BW precisely, use this:
+# -b:v 2M -maxrate 2M
 ffmpeg  \
     -hwaccel vaapi \
     -vaapi_device /dev/dri/renderD128 \
     -hwaccel_output_format vaapi \
     -i "$1" \
     -c:v h264_vaapi \
-    -b:v 2M \
-    -maxrate 2M \
+    -crf 23 \
     "$2"
