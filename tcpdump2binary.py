c0b1bd5c6acb1ca659c28ef0ed8a19db314836e9
Thanassis Tsiodras <thanassis@mathrix.com>
2024-05-14 15:48:58 +0200

    Flake8-ed

--- tcpdump2binary.py
+++ tcpdump2binary.py
@@ -1,17 +1,17 @@
-#!.scapy/bin/python3
+#!/home/thanassis/bin/.scapy/bin/python3
+import os
 import argparse
 
-from scapy.all import *
+from scapy.all import Ether, rdpcap
+
 
 def extract_eth_payloads(tcpdump_file, output_dir):
     os.makedirs(output_dir, exist_ok=True)
-
     packets = rdpcap(tcpdump_file)
-
     for i, packet in enumerate(packets):
         if Ether in packet:
             eth_header = bytes(packet[Ether])
-            filename = os.path.join(output_dir, f'frame_{i:05d}.bin')
+            filename = os.path.join(output_dir, f'frame_{i:06d}.bin')
             with open(filename, 'wb') as f:
                 f.write(eth_header)
 
@@ -19,15 +19,18 @@ def extract_eth_payloads(tcpdump_file, output_dir):
 def main():
     parser = argparse.ArgumentParser(
                     prog='tcpdump2binary',
-                    description='Convert tcpdump capture to separate binary ETH frames',
+                    description='Tcpdump capture to (many) binary ETH frames',
                     epilog='Uses scapy for the actual extraction.')
     parser.add_argument('filename', help='filename containing tcpdump capture')
-    parser.add_argument('-o', '--outputFolder', help='The folder to store the output ETH frames', default='extractedFrames')
+    parser.add_argument('-o', '--outputFolder',
+                        help='The folder to store the output ETH frames',
+                        default='extractedFrames')
     args = parser.parse_args()
 
     tcpdump_file = args.filename
     output_dir = args.outputFolder
     extract_eth_payloads(tcpdump_file, output_dir)
+
 
 if __name__ == "__main__":
     main()
