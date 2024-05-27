e9e16a1d954ca1e17549aae3195d2511d5092bbf
Thanassis Tsiodras <thanassis@mathrix.com>
2024-05-27 08:47:17 +0200

    GC old snap packages

---
+++ snap_remove_old_versions.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Removes old revisions of snaps
+# CLOSE ALL SNAPS BEFORE RUNNING THIS
+set -eu
+
+LANG=C snap list --all | awk '/disabled/{print $1, $3}' |
+    while read snapname revision; do
+        sudo snap remove "$snapname" --revision="$revision"
+    done
