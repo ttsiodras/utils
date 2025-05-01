a8c8031d34d4d52c6912ed690ad9fa2473f51875
Thanassis Tsiodras <thanassis@mathrix.com>
2025-05-01 10:48:19 +0200

    Dump all threads for a process

---
+++ dump_all_thread_callstacks_for_pid.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+[ -d /proc/"$1" ] || {
+    echo "No such process pid: $1"
+    exit 1
+}
+gdb -q -p "$1" -batch \
+    -ex "thread apply all bt" \
+    -ex "detach" \
+    -ex "quit" \
+    &> backtrace.log
