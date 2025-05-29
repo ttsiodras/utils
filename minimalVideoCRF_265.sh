707037e9b124c84ed33666b33fe28ad76d07c141
Thanassis Tsiodras <ttsiodras@gmail.com>
2025-05-29 16:30:11 +0200

    Improved behavior in bash loops with -nostdin

--- minimalVideoCRF_265.sh
+++ minimalVideoCRF_265.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ffmpeg -i "$1" -c:v libx265 -crf 25 -preset fast -c:a aac -b:a 128k /dev/shm/output265.mp4
+ffmpeg -nostdin -i "$1" -c:v libx265 -crf 25 -preset fast -c:a aac -b:a 128k /dev/shm/output265.mp4
