0ff90e0a2e0084dac8bb7ba33318fda80149b92e
Thanassis Tsiodras <ttsiodras@gmail.com>
2025-05-29 16:27:23 +0200

    Always transcode in /dev/shm/ for non-NVME impacting previewing of result

--- minimalVideoCRF_265.sh
+++ minimalVideoCRF_265.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ffmpeg -i "$1" -c:v libx265 -crf 28 -preset fast -c:a aac -b:a 128k output265.mp4
+ffmpeg -i "$1" -c:v libx265 -crf 25 -preset fast -c:a aac -b:a 128k /dev/shm/output265.mp4
