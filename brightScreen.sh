713cdc3c184c16103682a998c71b0c3660f20272
Thanassis Tsiodras <ttsiodras@gmail.com>
2025-05-29 16:16:43 +0200

    Adapt to new HW

--- brightScreen.sh
+++ brightScreen.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo -n "Max setting: "
-MAXBRIGHTNESS=$(cat /sys/class/backlight/intel_backlight/max_brightness)
+MAXBRIGHTNESS=$(cat /sys/class/backlight/*/max_brightness)
 echo $MAXBRIGHTNESS
 VAL=${1:-$MAXBRIGHTNESS}
-echo $VAL | sudo tee /sys/class/backlight/intel_backlight/brightness
+VAL=$(($VAL / 2))
+echo $VAL | sudo tee /sys/class/backlight/*/brightness
