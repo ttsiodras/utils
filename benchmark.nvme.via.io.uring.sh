1464298acfc065670671535dcb3c6069d4689d77
Thanassis Tsiodras <ttsiodras@gmail.com>
2025-06-28 15:51:45 +0200

    Use CDM compatible values

--- benchmark.nvme.via.io.uring.sh
+++ benchmark.nvme.via.io.uring.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 DEVICE=${1:-/dev/nvme0n1}
-fio                     \
-    --name=readtest     \
-    --filename=$DEVICE  \
-    --direct=1          \
-    --rw=read           \
-    --bs=256k           \
-    --iodepth=32        \
-    --numjobs=$(nproc)  \
-    --runtime=15        \
-    --time_based        \
-    --ioengine=io_uring \
+fio                      \
+    --name=readtest      \
+    --filename="$DEVICE" \
+    --direct=1           \
+    --rw=read            \
+    --bs=1M              \
+    --iodepth=8          \
+    --numjobs=1          \
+    --runtime=10         \
+    --time_based         \
+    --ioengine=io_uring  \
     --group_reporting
