94dc6c03d3d57050b83282814c2cf0de65e41eec
Thanassis Tsiodras <ttsiodras@gmail.com>
2025-08-31 17:34:38 +0200

    Use the full brightness (OLED screens)

--- brightScreen.sh
+++ brightScreen.sh
@@ -3,5 +3,5 @@ echo -n "Max setting: "
 MAXBRIGHTNESS=$(cat /sys/class/backlight/*/max_brightness)
 echo $MAXBRIGHTNESS
 VAL=${1:-$MAXBRIGHTNESS}
-VAL=$(($VAL / 2))
+# VAL=$(($VAL / 2))
 echo $VAL | sudo tee /sys/class/backlight/*/brightness
