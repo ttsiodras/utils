fa3de5a484694e1519bf4d0e4ac8e486eba6f2bd
Thanassis Tsiodras <ttsiodras@gmail.com>
2025-11-15 15:39:46 +0100

    Show symlinks, too

--- latest.sh
+++ latest.sh
@@ -3,4 +3,4 @@
 # UNIX power - makes latest.py obsolete.
 #
 DIR=${1:-.}
-find "${DIR}" -xdev -type f -printf "%T+ %11s %p\\n" | sort -n | grep -v vim/bundle
+find "${DIR}" -xdev ! -type d -printf "%T+ %11s %p\\n" | sort -n | grep -v vim/bundle
