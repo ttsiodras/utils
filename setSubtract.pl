4b5e59dd02dffbe969a151712ce3762f478bb86c
Thanassis Tsiodras <ttsiodras@gmail.com>
2025-11-30 14:24:05 +0100

    Add stable sort option

--- setSubtract.pl
+++ setSubtract.pl
@@ -4,19 +4,29 @@
 # (ignoring order, etc) - use this little script.
 #
 use strict;
-die "Usage: $0 bigSet smallSet\n" unless @ARGV == 2;
+use Getopt::Std;
+
+my %opts;
+getopts('s', \%opts);
+
+die "Usage: $0 [-s] bigSet smallSet\n" unless @ARGV == 2;
+
 my %a;
-open DATA1, $ARGV[0];
+open DATA1, $ARGV[0] or die "Cannot open $ARGV[0]: $!\n";
 while(<DATA1>) {
-	chomp;
-	$a{$_}=1;
+        chomp;
+        $a{$_}=1;
 }
 close DATA1;
-open DATA2, $ARGV[1];
+
+open DATA2, $ARGV[1] or die "Cannot open $ARGV[1]: $!\n";
 while(<DATA2>) {
-	chomp;
-	delete $a{$_};
+        chomp;
+        delete $a{$_};
 }
-foreach my $key (keys %a) {
-	print $key."\n";
+close DATA2;
+
+my @keys = $opts{s} ? sort keys %a : keys %a;
+foreach my $key (@keys) {
+        print $key."\n";
 }
