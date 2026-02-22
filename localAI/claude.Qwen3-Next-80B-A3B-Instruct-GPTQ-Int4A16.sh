b96ded3fc7562a30321146777be78419dc27d78b
Thanassis Tsiodras <ttsiodras@gmail.com>
2026-02-22 15:18:08 +0100

    Reuse common Dockerfile parts

--- localAI/claude.Qwen3-Next-80B-A3B-Instruct-GPTQ-Int4A16.sh
+++ localAI/claude.Qwen3-Next-80B-A3B-Instruct-GPTQ-Int4A16.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --network=restricted_net -w $PWD --rm -v $PWD:$PWD -it claude-qwen3-next-80b-a3b-instruct-gptq-int4a16 /bin/bash 
+docker run --network=restricted_net -w $PWD --rm -v $PWD:$PWD -it claude-qwen3-next-80b-a3b-instruct-gptq-int4a16
--- localAI/claude.gpt-oss-120b.sh
+++ localAI/claude.gpt-oss-120b.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --network=restricted_net -w $PWD --rm -v $PWD:$PWD -it claude-gpt-oss-120b /bin/bash 
+docker run --network=restricted_net -w $PWD --rm -v $PWD:$PWD -it claude-gpt-oss-120b
