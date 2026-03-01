7db2c6efda5e39ce464eaaafe2e885b7909703c9
Thanassis Tsiodras <ttsiodras@gmail.com>
2026-03-01 09:29:21 +0100

    Fwd inside the isolated Claude container

--- localAI/claude.gpt-oss-120b.sh
+++ localAI/claude.gpt-oss-120b.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+echo "[-] Remember to:"
+echo "    socat TCP-LISTEN:8000,reuseaddr,fork,bind=172.17.0.1 TCP:localhost:8000"
 docker run --network=restricted_net -w $PWD --rm -v $PWD:$PWD -it claude-gpt-oss-120b
