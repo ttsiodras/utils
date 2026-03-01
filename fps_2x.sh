7b8759e8db3ccea22aa4e020b3a88cbf7aae9ebd
Thanassis Tsiodras <ttsiodras@gmail.com>
2026-03-01 09:28:43 +0100

    Experimenting with VAAPI

--- fps_2x.sh
+++ fps_2x.sh
@@ -1,3 +1,3 @@
-ffmpeg -hwaccel vaapi -hwaccel_device /dev/dri/renderD128 -i "$1" \
+ffmpeg -vaapi_device /dev/dri/renderD128 -hwaccel vaapi -hwaccel_device /dev/dri/renderD128 -i "$1" \
   -vf "minterpolate=fps=60:mi_mode=mci:mc_mode=aobmc,format=nv12,hwupload" \
   -c:v hevc_vaapi -qp 23 /dev/shm/2x.mp4
