e98a92ae496bd703da853183325b7631d9f09bf4
Thanassis Tsiodras <ttsiodras@gmail.com>
2026-05-17 09:57:15 +0200

    No need to use kitty - xterm bug fixed

--- pi.google.sh
+++ pi.google.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-if [ -z "$KITTY_PID" ]; then
-    echo "[x] You are not inside kitty — pi depends on the Kitty protocol."
-    read -rp "[-] Shall I launch kitty and run pi there? [Y/n] " ANS
-    if [ "$ANS" = "n" ] || [ "$ANS" = "N" ]; then
-        echo "[-] Aborting."
-        exit 1
-    else
-        kitty bash "$0" "$@" &
-        exit 0
-    fi
-fi
+
+# No longer necessary, xterm bug fixed.
+#
+# if [ -z "$KITTY_PID" ]; then
+#     echo "[x] You are not inside kitty — pi depends on the Kitty protocol."
+#     read -rp "[-] Shall I launch kitty and run pi there? [Y/n] " ANS
+#     if [ "$ANS" = "n" ] || [ "$ANS" = "N" ]; then
+#         echo "[-] Aborting."
+#         exit 1
+#     else
+#         kitty bash "$0" "$@" &
+#         exit 0
+#     fi
+# fi
 
 source "${SCRIPT_DIR}"/ai.google.key || exit 1
 
