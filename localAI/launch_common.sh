b16f2a99e552ca648d4573ca2f24e9eb11c1808a
Thanassis Tsiodras <ttsiodras@gmail.com>
2026-06-08 19:58:11 +0200

    Use the QAT version of Gemma4/12B with MTP(2) (20% speedup)

--- localAI/launch_common.sh
+++ localAI/launch_common.sh
@@ -6,7 +6,8 @@ COMMON_ARGS="
   --offline
   --host 127.0.0.1
   --port 8081
-  --fit on
+  -ngl 99
+  --fit off
   --jinja
   --flash-attn on
   --presence-penalty 0.0
@@ -16,7 +17,10 @@ COMMON_ARGS="
 "
 
 GEMMA_ARGS="
-  --hf-repo unsloth/gemma-4-12b-it-GGUF:Q4_K_M
+  --hf-repo google/gemma-4-12B-it-qat-q4_0-gguf:Q4_0
+  --spec-draft-hf RachidAR/gemma-4-12B-it-qat-q4_0-MTP-assistant-gguf:Q4_0
+  --spec-type draft-mtp
+  --spec-draft-n-max 2
   --ctx-size 32768
   --temp 1.0
   --min-p 0.05
@@ -24,6 +28,7 @@ GEMMA_ARGS="
   --top-p 0.95
   --parallel 1
 "
+# --no-mmproj
 
 # Qwen3.5 recommended params from model card:
 # thinking mode:  temp=0.6, top-k=20, top-p=0.95, min-p=0.0
